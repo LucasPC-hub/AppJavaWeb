5d39b7a70abe93f2aca6b21e45270601856e5e61
Lucas <lucas.pc99@hotmail.com>
2023-02-12 01:44:50 -0300

    Inicio

---
+++ src/main/java/br/edu/infnet/appjavaweb/AppJavaWebApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appjavaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJavaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppJavaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/infnet/appjavaweb/AppJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appjavaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJavaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
